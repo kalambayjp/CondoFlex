fe7664529ccd626716f82333ab18ae30e7332e62
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2022-02-11 18:53:15 -0500

    Modify register form.

--- react-front-end/src/components/register.js
+++ react-front-end/src/components/register.js
@@ -3,17 +3,17 @@ import "./register.css"
 
 function Register() {
   return(
-    <form className="register">
+    <form className="register" action="/register" method="post">
       <h6>Register</h6>
 
-      <input placeholder="First Name"></input>
-      <input placeholder="Last Name"></input>
-      <input placeholder="Email"></input>
-      <input placeholder="Password"></input>
-      <input placeholder="Phone Number"></input>
-      <input placeholder="Building Code"></input>
-      <input placeholder="Unit Number"></input>
-
+      <input type="text" placeholder="First Name"></input>
+      <input type="text" placeholder="Last Name"></input>
+      <input type="text" placeholder="Email"></input>
+      <input type="password" placeholder="Password"></input>
+      <input type="text" placeholder="Phone Number"></input>
+      <input type="text" placeholder="Building Code"></input>
+      <input type="text" placeholder="Unit Number"></input>
+      <input type="submit" value="Submit"></input>
     </form>
   )
 }
