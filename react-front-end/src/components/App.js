47b46f40c67c4595fe6a04e6e7a0d4852466e6f2
Jp <kalambayjp@gmail.com>
2022-02-10 16:54:28 -0800

    started putting together a nav components

---
+++ react-front-end/src/components/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./App.css";
+import Nav from "./nav.js"
+
+function App() {
+  return (
+    <main>
+      <section>
+        
+        
+
+        <nav>
+          <Nav />
+        </nav>
+
+      </section>
+
+      <body>
+        pictures of registered builings?
+      </body>
+
+      <section >
+        footer
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ react-front-end/src/components/nav.js
@@ -0,0 +1,18 @@
+import React from "react"
+import "./nav.css";
+
+function Nav() {
+  return (
+    <nav>
+      <span>Logo</span>
+
+      <section>
+        <span>Login</span>
+        <span>Register</span>
+      </section>
+
+    </nav>
+  )
+}
+
+export default Nav
