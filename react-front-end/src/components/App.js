0d25bf9683b1a6af758e64f2b8ec718caf07b826
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2022-02-11 18:50:40 -0500

    Make HTTP Requests from React to Node

--- react-front-end/src/components/App.js
+++ react-front-end/src/components/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Nav from "./nav.js";
 import Footer from "./footer";
@@ -7,30 +7,47 @@ import Register from "./register";
 
 
 function App() {
-  const [state, setState] = useState([])
+  const [state, setState] = useState([]);
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api/data")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+
 
   return (
     <main>
+
       <nav>
-        <Nav state={state} setState= {setState} />
+        <Nav state={state} setState={setState} />
       </nav>
+
       <header>
         <Header />
       </header>
+
       <div className="page-content">
         <body>
-        </body>
-          <content>
-            
 
-            {state === "register" && <Register />}
-          
-            
-          </content>
+        </body>
+
+        <content>
+          {state === "register" && <Register />}
+        </content>
+
+        <div>
+          <p >{!data ? "Loading..." : data}</p>
+        </div>
+
       </div>
+
       <footer className="bottom">
         <Footer />
       </footer>
+
     </main>
   );
 }
