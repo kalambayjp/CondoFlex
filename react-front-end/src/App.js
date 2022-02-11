cbfe5859223b0d3a5c70d8fa86089194292f1c93
Ruonaclinton@gmail.com <ruonaclinton@yahoo.com>
2022-02-11 02:15:31 -0500

    Login page created, and styled with css

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Login from "./components/Login"
 
 function App() {
   return (
     <div>
+      <Login/>
       <h1>CondoFlex</h1>      
     </div>
   )
