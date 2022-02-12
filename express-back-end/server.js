45e4664d7eac39e53412e3d5a720fa1cdd733729
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2022-02-11 22:55:54 -0500

    Refactor the Server

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -2,36 +2,31 @@ const Express = require('express');
 const App = Express();
 const BodyParser = require('body-parser');
 const PORT = 8090;
+const db = require('./queries')
 
 // Express Configuration
-App.use(BodyParser.urlencoded({ extended: false }));
+
+App.use(BodyParser.urlencoded({ extended: true }));
 App.use(BodyParser.json());
 App.use(Express.static('public'));
 
-// Sample GET route
-App.get('/api/data', (req, res) => res.json(
-  {
-    message: "Seems to work!",
-  }));
 
 
-//
 
-App.get("/", (req, res) => {
-    db.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).json(results.rows)
-    })
+// Routes
 
-});
+App.get('/users', db.getUsers)
+App.get('/users/:id', db.getUserById)
+App.post('/users', db.createUser)
+App.put('/users/:id', db.updateUser)
+App.delete('/users/:id', db.deleteUser)
 
 
 
 
+
+// Listen Server Port
 
 App.listen(PORT, () => {
-  // eslint-disable-next-line no-console
   console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
 });
