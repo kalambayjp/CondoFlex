5ae8494a240b3b238b71312e87f5774a3a6a2c24
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2022-02-11 22:10:52 -0500

    Prevent to submit blank field

--- react-front-end/src/components/register.js
+++ react-front-end/src/components/register.js
@@ -13,37 +13,37 @@ function Register() {
         <label for="First Name" id="title_label">
           First Name :
         </label>
-        <input placeholder="First Name"></input>
+        <input placeholder="First Name" required></input>
 
         <label for="Last Name" id="title_label">
           Last Name :
         </label>
-        <input placeholder="Last Name"></input>
+        <input placeholder="Last Name" required></input>
 
         <label for="Email" id="title_label">
           Email :
         </label>
-        <input placeholder="Email"></input>
+        <input placeholder="Email" required></input>
 
         <label for="Password" id="title_label">
           Password :
         </label>
-        <input type="password" placeholder="Password"></input>
+        <input type="password" placeholder="Password" required></input>
 
         <label for="Phone Number" id="title_label">
           Phone Number :
         </label>
-        <input placeholder="Phone Number"></input>
+        <input placeholder="Phone Number" required></input>
 
         <label for="Building Code" id="title_label">
           Building Code :
         </label>
-        <input placeholder="Building Code"></input>
+        <input placeholder="Building Code" required></input>
 
         <label for="Unit Number" id="title_label">
           Unit Number :
         </label>
-        <input placeholder="Unit Number"></input>
+        <input placeholder="Unit Number" required></input>
 
         <button type="submit" id="btn_submit">
           Submit
