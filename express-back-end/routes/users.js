b0a94541d0837c15e6505e92151c859cd296eb6f
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2022-02-16 23:49:16 -0500

    Insert user's registration data into Database.

--- express-back-end/routes/users.js
+++ express-back-end/routes/users.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { PrismaClient } = require("@prisma/client");
+const { PrismaClient, Prisma  } = require("@prisma/client");
 const { users } = new PrismaClient();
 const BodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
@@ -20,40 +20,104 @@ router.get('/', async (req, res) => {
 })
 
 router.post('/login', async (req, res) => {
-  console.log("REQBODYFORM123",req.body.formDetails)
-  const {email, password}= req.body.formDetails
-  
+  console.log("REQBODYFORM123", req.body.formDetails)
+  const { email, password } = req.body.formDetails
 
-  console.log("PASSSSSSSSSUSERRRRRRRRRR",password);
+
+  console.log("PASSSSSSSSSUSERRRRRRRRRR", password);
   const usersData = await users.findUnique({
-    where:{
-      email:email,
+    where: {
+      email: email,
     }
   });
-  console.log("\n USERDATAAAAAAAAAA \n",usersData)
-  let Logged="";
-  let first_name=""
-  let building_code=""
+  console.log("\n USERDATAAAAAAAAAA \n", usersData)
+  let Logged = "";
+  let first_name = ""
+  let building_code = ""
 
   // console.log("PASSSSSSSSS",usersData.password)
   //console.log("PASSSSSSSSSUSERRRRRRRRRR",password);
-  
-  if((users)&&(usersData)&&(usersData.password===password)){
+
+  if ((users) && (usersData) && (usersData.password === password)) {
     first_name = usersData.first_name;
     building_code = usersData.building_code;
     // console.log("RRREQQQSESSION",req.session.user_id)
-    Logged="Successful";
+    Logged = "Successful";
     console.log("Valid")
   }
-  else{
-    Logged="Unsuccessful";
+  else {
+    Logged = "Unsuccessful";
     console.log("invalid")
   }
 
   // console.log("USERDATAAAAAA",usersData)
-  res.json({first_name: first_name,building_code: building_code, Logged:Logged });
-  
+  res.json({ first_name: first_name, building_code: building_code, Logged: Logged });
+
 })
 
 
+//Register 
+
+
+router.post('/create', async (req, res) => {
+  console.log("Register Data:", req.body.formData)
+  const { first_name, last_name, email, password, phone_number, unit_number, building_code } = req.body.formData
+
+  const firstName = first_name;
+  const lastName = last_name;
+  const userEmail = email;
+  const userPassword = password;
+  const phoneNumber = Number.parseInt(phone_number);
+  const unitNumber = Number.parseInt(unit_number);
+  const buildingCode = Number.parseInt(building_code);
+
+  try {
+    await users.create({
+      data: {
+        first_name: firstName,
+        last_name: lastName,
+        email: userEmail,
+        password: userPassword,
+        phone_number: phoneNumber,
+        unit_number: unitNumber,
+        building_code: buildingCode
+      },
+    })
+  } catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      if (e.code === 'P2002') {
+        console.log(
+          'There is a unique constraint violation, a new user cannot be created with this email.'
+        )
+      }
+    }
+    throw e
+  }
+
+  let userCreated = "";
+  let userId = 0;
+
+  const usersData = await users.findUnique({
+    where: {
+      email: userEmail,
+    }
+  });
+
+  if ((usersData) && (usersData.email === userEmail)) {
+    userId = usersData.id;
+    userCreated = "Successful";
+    console.log("User Created: ",)
+  }
+  else {
+    userCreated = "Unsuccessful";
+  }
+
+  res.json({ userCreated: userCreated, userId: userId });
+
+})
+
+
+
+
 module.exports = router
+
