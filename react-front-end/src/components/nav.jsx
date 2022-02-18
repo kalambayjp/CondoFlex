49506e81b3aa6e5a703d73a8c45ac1ca5c738135
Ruonaclinton@gmail.com <ruonaclinton@yahoo.com>
2022-02-17 19:45:25 -0500

    package json

--- react-front-end/src/components/nav.jsx
+++ react-front-end/src/components/nav.jsx
@@ -23,7 +23,7 @@ function Nav(props) {
     <nav className="navbar">
       <div className="container-fluid">
         <div id="logo">
-          <img src={logo} href="/home" alt="Condo Flex" />
+          <img src={logo} href="/" alt="Condo Flex" />
         </div>
         <section>
           <div>
